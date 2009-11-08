ebd2ebad88b68c95482205f3aee5fd7ca14d96a9
Matthias Rampke <matthias@rampke.de>
2009-11-08 13:16:28 +0100

    import StyleUtils dependency

---
+++ StyleUtils.cpp
@@ -0,0 +1,79 @@
+/* 
+   Implementation of ResourceUtils.h
+   Contains function for loading a resource by name and converting it to a BBitmap.
+   Distributed as part of the resource editor Style from Realize productionS.
+   There are absolutely no restrictions on the usage/re-distribution of this file.
+
+   		 Realize productionS
+   Superior software for a superior OS
+*/
+
+#include <be/app/Application.h>
+#include <be/interface/Bitmap.h>
+#include <be/storage/File.h>
+#include <be/storage/Resources.h>
+
+BBitmap *FetchStyleResource(const char *rcName)
+{
+	int i=0;
+	int32 id;
+	char *name;
+	size_t length;
+	type_code type;
+	void *data;
+	
+	// Initialize the file
+	app_info info;
+	
+	if (be_app->GetAppInfo(&info)!=B_OK)
+		return NULL;
+		
+	entry_ref ref=info.ref;
+	
+	BFile file(&ref, B_READ_ONLY);
+	
+	if (file.InitCheck()!=B_OK)
+		return NULL;
+		
+	BResources rcFile;
+	
+	if (rcFile.SetTo(&file)!=B_OK)
+		return NULL;
+		
+	// Search for resource
+	while(true)
+		if (rcFile.GetResourceInfo(i++, &type, &id, &name, &length))
+		{
+			if (strcmp(name, rcName)==0)
+				{
+					data=rcFile.LoadResource(type, name, &length);
+					break;
+				}			
+		}
+		else 
+			return NULL;
+
+	if (!data)
+		return NULL;
+		
+	// Instantiate BBitmap object from resource data
+	BMessage msg;
+	
+	if(msg.Unflatten((const char*)data)!=B_OK)
+		return NULL;
+		
+	BArchivable *unarchived = instantiate_object(&msg);
+	
+	if(unarchived)
+	{
+		BBitmap *bmp = NULL;
+		bmp=cast_as(unarchived, BBitmap);
+
+		if (bmp)
+			return bmp;
+		else
+			delete unarchived;
+	}
+	
+	return NULL;	
+}
---
+++ StyleUtils.h
@@ -0,0 +1,12 @@
+/* 
+   Definition of ResourceUtils.h
+   Contains function for loading a resource by name and converting it to a BBitmap.
+   Distributed as part of the resource editor Style from Realize productionS.
+   There are absolutely no restrictions on the usage/re-distribution of this file.
+
+   		 Realize productionS
+   Superior software for a superior OS
+*/
+
+BBitmap *FetchStyleResource(const char *rcName);
+
